c95796091e44a374d124109651882620392d73b2
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2018-11-02 12:29:13 +0100

    Coucou

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,23 @@
 package exercise1;
 
 class Person {
+
+	private int age;
+	private String nom;
+
+	public Person(String nom, int age)
+	{
+		this.nom = nom;
+		this.age = age;
+	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
 }
